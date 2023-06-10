9e90e580f1d086aba1ae606980b62f69f3723f15
unknown <e rlck3927@naver.com>
2023-06-10 11:10:48 +0900

    added 1_sum file

---
+++ src/1_sum.py
@@ -0,0 +1,20 @@
+"""
+설명: 1부터 10까지의 합
+"""
+
+result = 0
+
+for i in range(1, 11):
+    result = result + i
+
+# result = result + 1
+# result = result + 2
+# result = result + 3
+# result = result + 4
+# result = result + 5
+# result = result + 6
+# result = result + 7
+# result = result + 8
+# result = result + 9
+# result = result + 10
+print(result)
